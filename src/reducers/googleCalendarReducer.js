e431dc711fbd922cfc34ac696868c9248bb4e7eb
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-02-17 10:34:54 +0530

    Update googleCalendarReducer.js

--- src/reducers/googleCalendarReducer.js
+++ src/reducers/googleCalendarReducer.js
@@ -1,7 +1,10 @@
+// googleCalendarReducer.js
 import { SYNC_GOOGLE_CALENDAR } from '../actions/types';
 
 const initialState = {
   googleCalendarEvents: [],
+  googleAccessToken: null,
+  googleRefreshToken: null
 };
 
 export default function (state = initialState, action) {
@@ -12,8 +15,10 @@ export default function (state = initialState, action) {
         googleCalendarEvents: action.payload.syncedEvents.map(event => ({
           ...event,
           start: new Date(event.date),
-          end: new Date(event.date), // Again, adjust this based on duration
+          end: new Date(event.date),
         })),
+        googleAccessToken: action.payload.googleAccessToken || state.googleAccessToken,
+        googleRefreshToken: action.payload.googleRefreshToken || state.googleRefreshToken
       };
     default:
       return state;
