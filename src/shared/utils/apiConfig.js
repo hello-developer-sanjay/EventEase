f8f08b7104cfcac166b54cd8eb49a16cd01972b3
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-06-19 23:37:14 +0530

    Update apiConfig.js

--- src/shared/utils/apiConfig.js
+++ src/shared/utils/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
-  eventease:  'https://q0lvs5rnt9.execute-api.ap-south-1.amazonaws.com/prod/api',
-  eventpro:  'https://eventmanager-api-19july.onrender.com/api',
+  eventease: '/api/eventease',
+  eventpro: '/api/eventpro',
 };
 
 export default apiConfig;
