ac926aed2a05dcf6c218e28e18e66e5b90a4a7f5
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-06-20 22:28:58 +0530

    Update EditEventPage.jsx

--- src/eventpro/pages/EditEventPage.jsx
+++ src/eventpro/pages/EditEventPage.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import EventForm from '../components/EventForm';
 
 const EditEventPage = () => {
-    return (
-        <div>
-            <EventForm />
-        </div>
-    );
+  const navigate = useNavigate();
+  const { state } = useLocation();
+  const eventToEdit = state?.eventToEdit;
+
+  const clearEdit = () => {
+    navigate('/eventpro/list-events', { replace: true });
+  };
+
+  return (
+    <div>
+      <EventForm eventToEdit={eventToEdit} clearEdit={clearEdit} />
+    </div>
+  );
 };
 
 export default EditEventPage;
