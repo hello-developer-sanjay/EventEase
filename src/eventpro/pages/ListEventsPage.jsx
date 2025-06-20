58138f50e8afdcea95d2e1c8dea0b16cf431e79b
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-06-20 14:04:54 +0530

    Update ListEventsPage.jsx

--- src/eventpro/pages/ListEventsPage.jsx
+++ src/eventpro/pages/ListEventsPage.jsx
@@ -1,22 +1,49 @@
-import React, { useState } from 'react';
+import React from 'react';
 import EventTable from '../components/EventTable';
-import EventForm from '../components/EventForm'; // Import EventForm component
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 const ListEventsPage = () => {
-    const [editEvent, setEditEvent] = useState(null);
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const clearEdit = () => setEditEvent(null);
-
-    return (
-        <div>
-            <button onClick={() => navigate('/add-event')}>Add Event</button>
-            <EventTable setEditEvent={setEditEvent} />
-            {/* Render EventForm if editEvent is not null */}
-            {editEvent && <EventForm eventToEdit={editEvent} clearEdit={clearEdit} />}
-        </div>
-    );
+  return (
+    <PageWrapper>
+      <Header>
+        <Button onClick={() => navigate('/eventpro/add-event')}>Add Event</Button>
+      </Header>
+      <EventTable />
+    </PageWrapper>
+  );
 };
 
 export default ListEventsPage;
+
+const PageWrapper = styled.div`
+  background: url('https://sanjaybasket.s3.ap-south-1.amazonaws.com/background.webp') no-repeat center center fixed;
+  background-size: cover;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+`;
+
+const Header = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  padding: 20px;
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size:4px;
+  &:hover {
+    background-color: #45a049;
+  }
+`;
