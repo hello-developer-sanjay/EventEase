f584084cb0c3ae8428a8317ea2237ed6e82e10ae
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-06-20 09:49:47 +0530

    Update setAuthToken.js

--- src/eventease/utils/setAuthToken.js
+++ src/eventease/utils/setAuthToken.js
@@ -1,10 +1,9 @@
-// src/eventease/utils/setAuthToken.js
+import axios from 'axios';
+
 export const setAuthToken = (token) => {
   if (token) {
-    localStorage.setItem('eventeaseToken', token);
     axios.defaults.headers.common['x-auth-token'] = token;
   } else {
-    localStorage.removeItem('eventeaseToken');
     delete axios.defaults.headers.common['x-auth-token'];
   }
 };
