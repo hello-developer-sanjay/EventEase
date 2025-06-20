2ddd61221a847f6bedb875f83aff6962102b7fbb
Sanjay Patidar <143290631+hello-developer-sanjay@users.noreply.github.com>
2025-06-20 22:29:51 +0530

    Update setAuthToken.js

--- src/eventpro/utils/setAuthToken.js
+++ src/eventpro/utils/setAuthToken.js
@@ -3,7 +3,11 @@ import axios from 'axios';
 export const setAuthToken = (token) => {
   if (token) {
     axios.defaults.headers.common['x-auth-token'] = token;
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    console.log('setAuthToken.js - Set headers:', { 'x-auth-token': token, Authorization: `Bearer ${token}` });
   } else {
     delete axios.defaults.headers.common['x-auth-token'];
+    delete axios.defaults.headers.common['Authorization'];
+    console.log('setAuthToken.js - Cleared headers');
   }
 };
